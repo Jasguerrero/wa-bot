2317ef48651e5627dfcdb9774a4203df58ea65ff
Jesús S Guerrero <jesus_sguerrero@hotmail.com>
2024-12-07 22:39:03 -0600

    add commands command

---
+++ tibia/constants.js
@@ -0,0 +1,7 @@
+// API URL
+const TIBIA_API_URL = 'https://api.tibiadata.com/v4';
+
+// Export constants
+module.exports = {
+  TIBIA_API_URL
+};
--- tibia/responses.js
+++ tibia/responses.js
@@ -4,6 +4,9 @@
  * @returns {string} - The response message or an empty string if no valid command is found.
  */
 const axios = require('axios');
+const {
+    TIBIA_API_URL
+} = require('./tibia/constants');
 
 /**
  * Parses a message and fetches house data from TibiaData API.
@@ -15,44 +18,51 @@ const handleTibiaResponse = async (msg) => {
   const parts = msg.trim().split(/\s+/);
 
   // Check if the command is "!house" and has exactly 3 words
-  if (parts[0] === '!house' && parts.length >= 3) {
+  let response = ''
+  if (parts[0] === '!commands') {
+    response = 'Comandos: \n!house {world} {city} (ejemplo: !house pacera thais)\n'
+  }
+  else if (parts[0] === '!house' && parts.length >= 3) {
     const world = parts[1]; // World is always the second part
     const city = parts.slice(2).join(' ');
-
-    try {
-      // Make GET request to TibiaData API
-      const response = await axios.get(`https://api.tibiadata.com/v4/houses/${world}/${city}`);
-
-      // Check if HTTP status code is 200
-      if (response.status === 200 && response.data?.houses?.house_list) {
-        console.log(response.data)
-        const houseList = response.data.houses.house_list;
-
-        // Filter houses that are auctioned
-        const auctionedHouses = houseList.filter(house => house.auctioned === true);
-
-        if (auctionedHouses.length === 0) {
-          return `No auctioned houses found in ${city}, ${world}.`;
-        }
-
-        // Format auctioned houses into a readable list
-        const houseDetails = auctionedHouses.map(house => {
-          return `Name: ${house.name}, Rent: ${house.rent} gold, Size: ${house.size} SQM, Current Bid: ${house.auction.current_bid}, Time Left: ${house.auction.time_left}\n`;
-        }).join('\n');
-
-        return `Auctioned Houses in ${city}, ${world}:\n${houseDetails}`;
-      } else {
-        return `${world} and ${city} not found.`;
-      }
-    } catch (error) {
-      console.error('Error fetching house data:', error.message);
-      return `${world} and ${city} not found.`;
-    }
+    response = await getHousesDetail(world, city)
   }
 
   // Return an empty string if no valid command is found
-  return '';
+  return response;
 };
+
+const getHousesDetail = async (world, city) => {
+    try {
+        // Make GET request to TibiaData API
+        const response = await axios.get(`${TIBIA_API_URL}/houses/${world}/${city}`);
+  
+        // Check if HTTP status code is 200
+        if (response.status === 200 && response.data?.houses?.house_list) {
+          console.log(response.data)
+          const houseList = response.data.houses.house_list;
+  
+          // Filter houses that are auctioned
+          const auctionedHouses = houseList.filter(house => house.auctioned === true);
+  
+          if (auctionedHouses.length === 0) {
+            return `No auctioned houses found in ${city}, ${world}.`;
+          }
+  
+          // Format auctioned houses into a readable list
+          const houseDetails = auctionedHouses.map(house => {
+            return `Name: ${house.name}, Rent: ${house.rent} gold, Size: ${house.size} SQM, Current Bid: ${house.auction.current_bid}, Time Left: ${house.auction.time_left}\n`;
+          }).join('\n');
+  
+          return `Auctioned Houses in ${city}, ${world}:\n\n${houseDetails}`;
+        } else {
+          return `${world} and ${city} not found.`;
+        }
+      } catch (error) {
+        console.error('Error fetching house data:', error.message);
+        return `Error: ${world} and ${city} not found.`;
+      }
+} 
 
 const isGermanyTimeBetween10And11AM = () => {
     const now = new Date();
