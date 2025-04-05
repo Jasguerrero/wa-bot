9d7f3c7cfbdc53a428afe3519f175242bd6d9614
Jesús S Guerrero <jesus_sguerrero@hotmail.com>
2025-04-05 07:30:11 -0600

    download image first

--- utils/util.js
+++ utils/util.js
@@ -19,13 +19,21 @@ const sendPeriodicMessage = async (sock, chatIDs, redisClient) => {
         // If the response is different from what we have stored in Redis, send messages
         if (response !== lastResponse) {
             console.log('New boss information detected, sending messages...');
+            const imageResponse = await axios.get(imageUrl, {
+                responseType: 'arraybuffer',
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                    'Referer': 'https://www.tibia.com',
+                    'Accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8'
+                }
+            });
             
             const arr = Array.from(chatIDs);
             for (let i = 0; i < arr.length; i++) {
                 console.log(`Sending message to: ${arr[i]}`);
                 console.log(imageUrl);
                 await sock.sendMessage(arr[i], {
-                    image: { url: imageUrl },
+                    image: { stream: imageResponse.data },
                     caption: response
                 });
                 console.log('Message sent to:', arr[i]);
